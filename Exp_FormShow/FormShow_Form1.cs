5b801835cb97cca941030df8f1ff30fff469b4e9
wolfogre <wolfogre@qq.com>
2015-04-22 23:27:44 +0800

    added Exp_FormShow new file:   Exp_FormShow/App.config new file:   Exp_FormShow/Exp_FormShow.csproj new file:   Exp_FormShow/FormShow_Form1.Designer.cs new file:   Exp_FormShow/FormShow_Form1.cs new file:   Exp_FormShow/FormShow_Form1.resx new file:   Exp_FormShow/Program.cs new file:   Exp_FormShow/Properties/AssemblyInfo.cs new file:   Exp_FormShow/Properties/Resources.Designer.cs new file:   Exp_FormShow/Properties/Resources.resx new file:   Exp_FormShow/Properties/Settings.Designer.cs new file:   Exp_FormShow/Properties/Settings.settings

---
+++ Exp_FormShow/FormShow_Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exp_FormShow
+{
+    public partial class FormShow_Form1 : Form
+    {
+        private int new_form_count { get; set; }
+
+        public FormShow_Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FormShow_Form1_Load(object sender, EventArgs e)
+        {
+            new_form_count = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form new_form = new FormShow_Form1();
+            new_form.Text = Text + "-" + "无模" + ++new_form_count;
+            new_form.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form new_form = new FormShow_Form1();
+            new_form.Text = Text + "-" + "有模" + ++new_form_count;
+            new_form.ShowDialog();
+        }
+    }
+}
